024e3f2c689fed8d6cb26fb92096c800d04e5242
action jackson <action@localhost.localdomain>
2017-04-04 16:32:32 -0700

    fork tgz-modify remove write-streams update readme

--- index.js
+++ index.js
@@ -3,41 +3,30 @@ const path = require('path')
 const fs = require('fs')
 const tar = require('tar-stream')
 
-module.exports = function (in_file, out_file, callback) {
+module.exports = function (in_file, callback) {
 
   if (!in_file) { throw 'no input file specified' }
-  if (!out_file) { throw 'no output file specified' }
+  if (!callback) { throw 'no callback was specified' }
 
   let input = fs.createReadStream(in_file)
 
   const gunzip = zlib.createGunzip()
-  const gzip = zlib.createGzip()
-  const pack = tar.pack()
   const extract = tar.extract()
 
   input.pipe(gunzip) // unzip the tar
     .pipe(extract) // parse the tar
     .on('entry', (header, stream, next) => {
       let file = header.name
-      let data_in = ''
-      stream.on('data', (d) => data_in += new Buffer(d, 'utf8').toString())
+      let data = ''
+      stream.on('data', (d) => data += new Buffer(d, 'utf8').toString())
       stream.on('end', () => {
-        let data_out = callback(header, data_in)
-        if (data_out) {
-          stream.pipe(pack.entry(header, data_out, next))
-        } next()
+        callback(header, data)
+        next()
       })
-      // }
-    })
-    .on('finish', () => {
-      pack.finalize() // >> .tar
     })
 
   input.on('finish', () => {
     input.close()
-    // let outstream = new fs.createWriteStream(out_file)
-    pack.pipe(gzip) // >> .tgz
-      .pipe(new fs.createWriteStream(out_file, {autoClose: true})) // >> outfile.tgz
   })
 }
 
